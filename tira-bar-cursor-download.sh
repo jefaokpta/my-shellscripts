31a87c6af07444bbb8e52d579be93ef7e4f30241
jefaokpta <jefaokpta@hotmail.com>
2025-03-24 10:30:52 -0300

    Atualiza URL de download do Cursor IDE para a versão 0.47.9

--- tira-bar-cursor-download.sh
+++ tira-bar-cursor-download.sh
@@ -2,7 +2,7 @@
 set -ex
 
 # Download Cursor IDE
-wget https://downloader.cursor.sh/linux/appImage/x64 -O ./Cursor_orig.AppImage
+wget https://downloads.cursor.com/production/b6fb41b5f36bda05cab7109606e7404a65d1ff32/linux/x64/Cursor-0.47.9-x86_64.AppImage -O ./Cursor_orig.AppImage
 chmod +x ./Cursor_orig.AppImage
 
 # Extract the AppImage
