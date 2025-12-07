c7773a5e868166fc916c143fe530414f6a20866a
jefaokpta <jefaokpta@hotmail.com>
2025-12-07 12:38:05 -0300

    Refactor timeshift snapshot backup script for improved readability and structure

--- timeshift-snap-backup.sh
+++ timeshift-snap-backup.sh
@@ -1,19 +1,32 @@
 #!/bin/bash
 
-snapshot=$1
-if [ -z "$snapshot" ]
+hdLocal="/dev/nvme0n1p2"
+hdExterno="/run/media/jefaokpta/TIMESHIFT"
+montagemLocal="/mnt"
+
+nomeSnapshot=$1
+if [ -z "$nomeSnapshot" ]
 then
-  echo "🧨 Falta o caminho completo do snapshot!"
+  echo "🧨 Falta o nome do snapshot!"
   exit 1
 fi
 
 ## Montar subvolumes do BTRFS
-sudo mount -o subvolid=5 /dev/nvme0n1p2 /mnt 
+echo "🔧 Montando HD local..."
+sudo mount -o subvolid=5 $hdLocal $montagemLocal
 
 ## Tornando snapshot read only
-sudo btrfs subvolume snapshot -r fonte /run/media/jefaokpta/TIMESHIFT
+echo "📦 Criando snapshot read only..."
+sudo btrfs subvolume snapshot -r $montagemLocal/timeshift/snapshots/$nomeSnapshot/@ $montagemLocal/timeshift/snapshots/${nomeSnapshot}_ro
 
+## Sincronizando dados BTRFS
+echo "⚡ Sincronizando dados..."
+sudo sync
 
+## Copiando snapshot para HD externo
+echo "🚚 Copiando snapshot para HD externo..."
+sudo btrfs send $montagemLocal/timeshift/snapshots/${nomeSnapshot}_ro | sudo btrfs receive $hdExterno
 
 ## Desmontando HD local
+echo "🔌 Desmontando HD local..."
 sudo umount /mnt
